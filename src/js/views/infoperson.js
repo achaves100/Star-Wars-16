6756f591a2af4ffea745f8505af9dd08f116d7d2
achaves100 <adan.chaves@outlook.com>
2021-03-26 03:01:48 +0000

    SW8 - Ipsum

--- src/js/views/infoperson.js
+++ src/js/views/infoperson.js
@@ -3,16 +3,28 @@ import { Link } from "react-router-dom";
 
 export const InfoPerson = () => (
 	<div className="text-center mt-5">
-		<h1>STAR WARS</h1>
 		<span>
 			<Link to="/persons">
 				<img
 					width={400}
 					height={300}
-					src="https://www.teahub.io/photos/full/68-686120_star-wars-death-star-background.jpg"
+					src="https://www.projectcasting.com/wp-content/uploads/2018/09/star-wars-696x400.jpg"
 					alt="Planets"
 				/>
 			</Link>
+		</span>
+		<span>
+			<h6>
+				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+				rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+				explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+				consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+				dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+				incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+				exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+				vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+				qui dolorem eum fugiat quo voluptas nulla pariatur?
+			</h6>
 		</span>
 	</div>
 );
