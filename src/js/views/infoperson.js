00319a685a4251c50f9d7b801974373871071cfd
achaves100 <adan.chaves@outlook.com>
2021-03-26 02:37:32 +0000

    SW8 - 3rd Pages - Pending Planets Count

--- src/js/views/infoperson.js
+++ src/js/views/infoperson.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export const InfoPerson = () => {
-	return <div>Hello InfoPerson</div>;
-};
+export const InfoPerson = () => (
+	<div className="text-center mt-5">
+		<h1>STAR WARS</h1>
+		<span>
+			<Link to="/persons">
+				<img
+					width={400}
+					height={300}
+					src="https://www.teahub.io/photos/full/68-686120_star-wars-death-star-background.jpg"
+					alt="Planets"
+				/>
+			</Link>
+		</span>
+	</div>
+);
--- src/js/views/infoplanet.js
+++ src/js/views/infoplanet.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export const InfoPlanet = () => {
-	return <div>Hello InfoPlanet</div>;
-};
+export const InfoPlanet = () => (
+	<div className="text-center mt-5">
+		<h1>STAR WARS</h1>
+		<span>
+			<Link to="/planets">
+				<img
+					width={400}
+					height={300}
+					src="https://www.teahub.io/photos/full/68-686120_star-wars-death-star-background.jpg"
+					alt="Planets"
+				/>
+			</Link>
+		</span>
+	</div>
+);
