eead0890e663527b3f0df1e543ca8e5cb7970a7e
achaves100 <adan.chaves@outlook.com>
2021-03-28 03:03:06 +0000

    SW10  - Added Table - Not down

--- src/js/views/infoperson.js
+++ src/js/views/infoperson.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const InfoPerson = () => (
-	<div className="text-center mt-5 d-flex text-left w-50 m-auto">
+	<div className="text-center mt-5 d-flex text-left w-50 m-auto col-6">
 		<Link to="/persons">
 			<img
 				width={400}
@@ -11,17 +11,46 @@ export const InfoPerson = () => (
 				alt="Planets"
 			/>
 		</Link>
-
-		<h6 className="text-left ml-3">
+		<h6 className="text-left ml-3 col-6">
 			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
 			aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
 			Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
 			dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
 			sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-			magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-			suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-			ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-			nulla pariatur?
+			magnam aliquam quaerat voluptatem.
 		</h6>
+		<div>
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">First</th>
+						<th scope="col">Last</th>
+						<th scope="col">Handle</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		;
 	</div>
 );
