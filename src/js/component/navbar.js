93d6b6ed100d96f00e489512b0d986045c37e981
achaves100 <adan.chaves@outlook.com>
2021-04-08 21:57:54 +0000

    Favorites showing - Pending Store

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,31 +11,24 @@ export const NavbarMenu = () => {
 	};
 
 	const ShowFavorites = () => {
-		if (store.favorites) {
-			return store.favorites.map((item, index) => {
-				return (
-					<Dropdown.Item key={index}>{item}</Dropdown.Item>
-					// <a className="dropdown-item" href="#" key={index}>
-					// 	{item}
-
-					// 	<button
-					// 		type="button"
-					// 		className="btn btn-dark float-right"
-					// 		onClick={e => {
-					// 			DeleteFavorite(index);
-					// 		}}>
-					// 		<i className="fas fa-trash" />
-					// 	</button>
-					// </a>
-				);
-			});
-		} else {
-			return <Dropdown.Item>Sin favoritos</Dropdown.Item>;
-		}
+		return store.favorites.map((item, index) => (
+			<Dropdown.Item key={index}>
+				{item}
+				<button
+					type="button"
+					className="btn btn-dark float-right"
+					onClick={e => {
+						DeleteFavorite(index);
+					}}>
+					<i className="fas fa-trash" />
+				</button>
+			</Dropdown.Item>
+		));
 	};
 
 	return (
 		<Container fluid>
+			<span>{JSON.stringify(store.favorites)}</span>
 			<Navbar bg="dark" text-white>
 				<Link to="/">
 					<img
@@ -47,32 +40,11 @@ export const NavbarMenu = () => {
 				</Link>
 
 				<Navbar.Collapse className="justify-content-end">
-					<DropdownButton variant="Primary" title="Favorites">
-						{ShowFavorites}
+					<DropdownButton menuAlign="right" variant="Primary" title="Favorites">
+						{ShowFavorites()}
 					</DropdownButton>
 				</Navbar.Collapse>
 			</Navbar>
 		</Container>
 	);
 };
-
-{
-	/* <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-</DropdownButton> */
-}
-
-{
-	/* <Dropdown>
-	<Dropdown.Toggle variant="success" id="dropdown-basic">
-		Dropdown Button
-	</Dropdown.Toggle>
-	<Dropdown.Menu>
-		<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-		<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-		<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-	</Dropdown.Menu>
-</Dropdown>; */
-}
